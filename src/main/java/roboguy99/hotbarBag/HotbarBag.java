9fc6c25c8564ac3c610eec9fc00df983d6973f28
robog <robog@JAKEDESKTOP.Home>
2016-01-24 19:08:46 +0000

    Updated version number

--- src/main/java/roboguy99/hotbarBag/HotbarBag.java
+++ src/main/java/roboguy99/hotbarBag/HotbarBag.java
@@ -37,7 +37,7 @@ public class HotbarBag
 {
 	// Mod data. Fallback if mc-mod.info fails to load.
 	public static final String modID = "HotbarBag";
-	public static final String modVersion = "1.0.2";
+	public static final String modVersion = "1.0.3";
 	public static final String name = "Hotbar Bag";
 	
 	public static final Logger logger = LogManager.getLogger("Hotbar Bag");
